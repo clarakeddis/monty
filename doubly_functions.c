6fda0c31f76384e9c0ba166514abdc270cd7fc40
JanKadry <157895334+JanKadry@users.noreply.github.com>
2024-09-26 22:44:52 +0300

    Update doubly_functions.c

--- doubly_functions.c
+++ doubly_functions.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- *add_dnodeint_end - add a note at the end of the doubly link list
+ *add_dnodeint_end - add note at the end of the doubly link list
  *@head: first position of linked list
  *@n: data to store
  *Return: a doubly linked list
